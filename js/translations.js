e2ff4394e1a924f0cb537f339b6b9e09fb31a9e5
Omar Abdelrahman <omarahmed8k@gmail.com>
2022-10-02 06:07:53 +0200

    Lang

---
+++ js/translations.js
@@ -0,0 +1,28 @@
+const translations = {
+  en: {
+    lang: "English",
+    home: "Home",
+    about: "About",
+    services: "Services",
+    projects: "Projects",
+    contact: "Contact",
+    rights: "All rights reserved © 2022",
+    selectLanguage: "Select a language",
+    english: "English",
+    arabic: "Arabic",
+  },
+  ar: {
+    lang: "عربي",
+    home: "الرئيسية",
+    about: "نحن",
+    services: "خدماتنا",
+    projects: "مشاريعنا",
+    contact: "تواصل معنا",
+    rights: "جميع الحقوق محفوظة © 2022",
+    selectLanguage: "إختر لغة",
+    english: "الانجليزية",
+    arabic: "العربية",
+  },
+};
+
+export default translations;
