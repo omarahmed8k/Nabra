55028a693f0a82090657863b2a2ceb4132c71d62
Omar Abdelrahman <153508387+omarwfd@users.noreply.github.com>
2023-12-31 02:27:39 +0200

    Add team section with images and hover effects

--- js/scripts.js
+++ js/scripts.js
@@ -76,6 +76,28 @@
         },
       },
     });
+
+    // Owl Carousel
+    $(".team").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 2,
+        },
+      },
+    });
   });
 
   // Wow animations
